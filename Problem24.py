cafa39571a66f3ff02f80819f995703893e8a768
sf4436904-art <sf4436904@gmail.com>
2026-06-23 07:32:10 +0530

    new

---
+++ Problem24.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        res = int(str(abs(x))[::-1])
+        if x < 0:
+            res = -res
+            
+        
+        if res < -2**31 or res > 2**31 - 1:
+            return 0
+            
+        return res
